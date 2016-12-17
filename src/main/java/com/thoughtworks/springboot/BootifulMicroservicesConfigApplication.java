123bafbc979791d3db5035f2946f1a0b37d7eb00
Rajat Bhatnagar <rajat_bhatnagar@outlook.com>
2016-12-17 10:22:46 -0500

    Refactor: Initial commit for a Configuration Server using Spring Boot

---
+++ src/main/java/com/thoughtworks/springboot/BootifulMicroservicesConfigApplication.java
@@ -0,0 +1,12 @@
+package com.thoughtworks.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootifulMicroservicesConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootifulMicroservicesConfigApplication.class, args);
+	}
+}
