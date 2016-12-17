9e0c3637c3d6f9075803905994458eda6e2f9ff2
Rajat Bhatnagar <rajat_bhatnagar@outlook.com>
2016-12-17 12:31:33 -0500

    Refactor: Added @EnableConfigServer to create it as a configuration server and added properties

--- src/main/java/com/thoughtworks/springboot/BootifulMicroservicesConfigApplication.java
+++ src/main/java/com/thoughtworks/springboot/BootifulMicroservicesConfigApplication.java
@@ -2,7 +2,9 @@ package com.thoughtworks.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+@EnableConfigServer
 @SpringBootApplication
 public class BootifulMicroservicesConfigApplication {
 
